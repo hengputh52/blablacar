5fe8fb698ddf1758d16dc900df737efd3121a737
hengputh52 <harrystyles10203040@gmail.com>
2026-02-05 11:26:50 +0700

    first commit

---
+++ test/lib/services/rides_service.dart
@@ -0,0 +1,17 @@
+import '../data/dummy_data.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+
+  // TODO Create a static method to filter rides by departure location
+
+  // TODO Create a static method to filter rides by number of requested seat
+
+  // TODO Create a static method to filter : optional departure location, optional requested seat
+
+}
