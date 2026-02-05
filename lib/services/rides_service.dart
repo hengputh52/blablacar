51825bc500701a591ada4a16ccf608aff98a46b7
hengputh52 <harrystyles10203040@gmail.com>
2026-02-05 21:00:54 +0700

    Bla-001

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -10,15 +10,23 @@ class RidesService {
   static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
   static List<Ride> filterByDeparture(Location departure) {
-    return availableRides.where((avaiableRide) => avaiableRide.departureLocation.name == departure.name).toList();
-  }
-    static List<Ride> filterBySeatRequested(int seatRequest) {
-    return availableRides.where((seat) => seat.remainingSeats >= seatRequest).toList();
-  }
-    static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    return availableRides.where((ride)= > ride.);
+    return availableRides
+        .where((avaiableRide) =>
+            avaiableRide.departureLocation.name == departure.name,
+        )
+        .toList();
   }
 
+  static List<Ride> filterBySeatRequested(int seatRequest) {
+    return availableRides
+        .where((seat) => seat.remainingSeats >= seatRequest)
+        .toList();
+  }
 
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
 
+    return availableRides
+    .where((filter) => (filter.departureLocation.name == departure!.name) && (filter.remainingSeats >= seatRequested!))
+    .toList();
+  }
 }
